a18c38dcea3e4f9155db9cc863868b9dc7e9132e
- width : Integer     - length : Integer     + Graph(width, length) <jordan.pereira@etu.unistra.fr>
2021-07-10 14:17:23 +0200

    Utilisation de smartinv

--- src/main/java/fr/mrwarzo/randomchest/inventory/ChestLocationMenu.java
+++ src/main/java/fr/mrwarzo/randomchest/inventory/ChestLocationMenu.java
@@ -5,8 +5,11 @@ import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
 import fr.minuskube.inv.content.SlotPos;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -20,20 +23,12 @@ public class ChestLocationMenu implements InventoryProvider {
 
     @Override
     public void init(Player player, InventoryContents contents) {
-        for(int i = 0; i < INVENTORY.getColumns(); i++) {
-            for(int j = 0; j < INVENTORY.getRows(); j++) {
-                if(i != 5 && j != 9) {
-                    contents.fill(ClickableItem.of(new ItemStack(Material.GRAY_STAINED_GLASS_PANE),
-                            e -> putChest(player, contents, e.getSlot())
-                    ));
-                }
-                else {
-                    contents.set(6, 5, ClickableItem.of(new ItemStack(Material.YELLOW_STAINED_GLASS_PANE),
-                            e -> player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_HURT, 100, 1)
-                    ));
-                }
-            }
-        }
+        contents.fill(ClickableItem.of(new ItemStack(Material.GRAY_STAINED_GLASS_PANE),
+                e -> putChest(player, contents, e.getSlot())
+        ));
+        contents.set(5, 4, ClickableItem.of(new ItemStack(Material.YELLOW_STAINED_GLASS_PANE),
+                e -> player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_HURT, 100, 1)
+        ));
     }
 
     @Override
@@ -42,13 +37,42 @@ public class ChestLocationMenu implements InventoryProvider {
     }
 
     public void putChest(Player player, InventoryContents contents, int slot) {
-        int x = slot / 9;
-        int y = slot % 9;
+        int x = slot % 9;
+        int z = slot / 9;
+
+        Location location = player.getLocation().toBlockLocation();
+        World world = location.getWorld();
 
         player.sendMessage(player.getFacing().toString());
-        contents.set(new SlotPos(x, y), ClickableItem.of(new ItemStack(Material.BROWN_STAINED_GLASS_PANE),
-                e -> player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_HURT, 100, 1)
-        ));
 
+        if(player.getFacing() == BlockFace.EAST || player.getFacing() == BlockFace.WEST) {
+            x -= 4;
+            z -= 5;
+
+            int temp = x;
+            x = z;
+            z = temp;
+            z *= -1;
+
+            if(player.getFacing() == BlockFace.EAST) {
+                x *= -1;
+                z *= -1;
+            }
+        }
+        else {
+            x -= 4;
+            z -= 5;
+            if(player.getFacing() == BlockFace.SOUTH) {
+                x *= -1;
+                z *= -1;
+            }
+        }
+
+        player.sendMessage("x=" + x);
+        player.sendMessage("z=" + z);
+
+        world.getBlockAt(location.add(x,0,z)).setType(Material.CHEST);
+
+        INVENTORY.close(player);
     }
 }
